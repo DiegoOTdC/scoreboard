bd8bd473a35fb2241715cac08604e48cc1791512
DiegoOTdC <studioartdiego@gmail.com>
2020-07-01 21:37:54 +0200

    removed setPlayers

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -3,7 +3,7 @@ import Player from "./Player";
 import AddPlayerForm from "./AddPlayerForm";
 
 export default function Scoreboard() {
-  const [players, set_players] = useState([
+  const [players] = useState([
     { id: 1, name: "Violeta", score: 11 },
     { id: 2, name: "Eszter", score: 14 },
     { id: 3, name: "Jeroen v2", score: 4 },
