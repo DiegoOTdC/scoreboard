3eb58c421502ce1965d6cdc80019e1824912a27e
DiegoOTdC <studioartdiego@gmail.com>
2020-07-02 14:53:40 +0200

    added sorting, increment, rest and randomize options

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 export default function Player(props) {
+  const onClickIncrement = () => {
+    props.incrementScore(props.id);
+  };
+
   return (
     <div>
-      <p key={props.id}>
-        {props.id} {props.name} {props.score}
-      </p>
+      <ul key={props.id}>
+        <li>
+          {props.name} (score: {props.score}){" "}
+          <button onClick={onClickIncrement}>increment</button>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -2,29 +2,93 @@ import React, { useState } from "react";
 import Player from "./Player";
 import AddPlayerForm from "./AddPlayerForm";
 
+function compare_score(player_a, player_b) {
+  return player_b.score - player_a.score;
+}
+
+function compare_name(player_a, player_b) {
+  return player_a.name.localeCompare(player_b.name);
+}
+
 export default function Scoreboard() {
-  const [players] = useState([
+  const [players, set_players] = useState([
     { id: 1, name: "Violeta", score: 11 },
     { id: 2, name: "Eszter", score: 14 },
     { id: 3, name: "Jeroen v2", score: 4 },
     { id: 4, name: "Lisa", score: 42 },
   ]);
 
+  const players_sorted = [...players];
+
+  const [sort_by, set_sort_by] = useState("score");
+  if (sort_by === "score") {
+    players_sorted.sort(compare_score);
+  } else {
+    players_sorted.sort(compare_name);
+  }
+
+  const change_sorting = (event) => {
+    console.log("new sort order:", event.target.value);
+    set_sort_by(event.target.value);
+  };
+
+  const incrementScore = (id) => {
+    set_players(
+      players.map((player) => {
+        if (player.id === id) {
+          return {
+            ...player,
+            score: player.score + 1,
+          };
+        } else {
+          return player;
+        }
+      })
+    );
+  };
+
+  const reset = () => {
+    set_players(
+      players.map((player) => {
+        return { ...player, score: (player.score = 0) };
+      })
+    );
+  };
+
+  const randomize = () => {
+    set_players(
+      players.map((player) => {
+        return {
+          ...player,
+          score: (player.score = Math.round(Math.random() * 100)),
+        };
+      })
+    );
+  };
+
   return (
     <div className="Scoreboard">
-      <h1>Scoreboard</h1>
-      <h2>
-        {players.map((player) => {
-          return (
-            <Player
-              key={player.id}
-              id={player.id}
-              name={player.name}
-              score={player.score}
-            />
-          );
-        })}
-      </h2>
+      <h4>Players scores:</h4>
+      <p>
+        Sort order:{" "}
+        <select onChange={change_sorting}>
+          <option value="score">Sort by score</option>
+          <option value="name">Sort by name</option>
+        </select>{" "}
+        <button onClick={reset}>reset</button>
+        <button onClick={randomize}>randomize</button>
+      </p>
+      {players_sorted.map((player) => {
+        return (
+          <Player
+            key={player.id}
+            id={player.id}
+            name={player.name}
+            score={player.score}
+            incrementScore={incrementScore}
+          />
+        );
+      })}
       <AddPlayerForm />
     </div>
   );
